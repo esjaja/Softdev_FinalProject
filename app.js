ce2d4e126d90c421f5e99c53a8e5e107be36dac5
shir231ms <shir231ms@yahoo.com.tw>
2016-11-30 20:54:08 +0800

    merge master branch app.js.

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-var express = require('express');
 var multiparty = require('connect-multiparty')();
+ar assert = require('assert');
+var fs = require('fs');
+var Gridfs = require('gridfs-stream');
+var loop = require('node-while-loop');
+var express = require('express');
 var path = require('path');
 //var favicon = require('serve-favicon');
 //var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+//var redisStore = require('connect-redis')(session);
 var expressValidator = require('express-validator');
 var mongoose = require('mongoose');
-var assert = require('assert');
-var fs = require('fs');
-var Gridfs = require('gridfs-stream');
-var loop = require('node-while-loop');
 
 var app = express();
 var router = express.Router();
 var uri = 'mongodb://softdev:softdev@ds157487.mlab.com:57487/softdev_db';
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,6 +24,14 @@ app.set('view engine', 'hjs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'somesecrettoken',
+    name: 'softdev'
+}));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
 app.use('/semantic', express.static(path.join(__dirname, 'semantic')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
@@ -48,8 +54,11 @@ var ActivityCtrl = require('./controller/ActivityCtrl.js');
 var UserCtrl = require('./controller/UserCtrl.js');
 // APIS
 app.get('/', UserCtrl.display_login);
+app.get('/logout', UserCtrl.logout);
 app.get('/index', ActivityCtrl.display_index);
 app.get('/activity', ActivityCtrl.display_activity);
+app.all('/login', UserCtrl.login);
+app.post('/create_activity', ActivityCtrl.create_activity);
 
 //router.post('/activity', multiparty, function(req, res){
 db.once('open', function () {
@@ -98,15 +107,14 @@ db.once('open', function () {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 
 app.listen(8080, () => {
   console.log("Start listening on " + 8080);
